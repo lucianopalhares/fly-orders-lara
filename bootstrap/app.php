34c91a15b761bb69ff0a521715c970267ff3fff6
L. Palharês Rosa <lucianopalharesrosa@hotmail.com>
2025-02-02 19:30:30 -0300

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,11 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Request;
-use Illuminate\Auth\AuthenticationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         api: __DIR__.'/../routes/api.php',
+        web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
