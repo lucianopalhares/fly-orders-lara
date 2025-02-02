292949635ab20da0cda5c8b89b1201bf481c86df
L. Palharês Rosa <lucianopalharesrosa@hotmail.com>
2025-02-02 15:01:51 -0300

    auth jwt working

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Middleware\JwtMiddleware;
 
-Route::prefix('/')->group(function () {
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware([JwtMiddleware::class])->group(function () {
+    Route::get('user', [AuthController::class, 'getUser']);
+    Route::post('logout', [AuthController::class, 'logout']);
+
     Route::prefix('orders')->group(function () {
         Route::post('/create', [OrderController::class, 'create']);
         Route::get('/show/{id}', [OrderController::class, 'get']);
